485a8934eec8ecac2b31b06f04d89cb77ef45100
ale-bast <17410941+ale-bast@users.noreply.github.com>
2024-01-08 14:05:29 +0100

    added font DsCeltic2.ttf

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -97,11 +97,7 @@ myTerminal = "alacritty"    -- Sets default terminal
 myBrowser :: String
 myBrowser = "qutebrowser "  -- Sets qutebrowser as browser
 
-myEmacs :: String
-myEmacs = "emacsclient -c -a 'emacs' "  -- Makes emacs keybindings easier to type
-
 myEditor :: String
--- myEditor = "emacsclient -c -a 'emacs' "  -- Sets emacs as editor
 myEditor = myTerminal ++ " -e vim "    -- Sets vim as editor
 
 myBorderWidth :: Dimension
