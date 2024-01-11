541afb18af73161b3c5b295edaa3d3a6d90f79bb
ale-bast <17410941+ale-bast@users.noreply.github.com>
2024-01-11 05:20:44 +0100

    alacritty dynamic_padding

--- .config/nvim/lua/alebast/packer.lua
+++ .config/nvim/lua/alebast/packer.lua
@@ -8,22 +8,28 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        -- or,
+        -- branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-    use({
+    use {
         'rose-pine/neovim',
         as = 'rose-pine',
         config = function()
-            vim.cmd('colorscheme rose-pine')
+            require('rose-pine').setup()
         end
-    })
+    }
 
-    use {"akinsho/toggleterm.nvim", tag = '*', config = function()
-        require("toggleterm").setup()
-    end}
+    use {
+        "akinsho/toggleterm.nvim",
+        tag = '*',
+        config = function()
+            require("toggleterm").setup()
+        end
+    }
 
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use('nvim-treesitter/playground')
